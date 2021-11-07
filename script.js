90e6c6be9908f66604cfa8047a7880e4670b1180
acmcalvo <91921941+acmcalvo@users.noreply.github.com>
2021-11-06 20:16:30 -0400

    change var passwor function

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+// GIVEN I need a new, secure password
+// WHEN I click the button to generate a password
+// THEN I am presented with a series of prompts for password criteria
+// WHEN prompted for password criteria
+// THEN I select which criteria to include in the password
+// WHEN prompted for the length of the password
+// THEN I choose a length of at least 8 characters and no more than 128 characters
+// WHEN asked for character types to include in the password
+// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+// WHEN I answer each prompt
+// THEN my input should be validated and at least one character type should be selected
+// WHEN all prompts are answered
+// THEN a password is generated that matches the selected criteria
+// WHEN the password is generated
+// THEN the password is either displayed in an alert or written to the page
+
 // Assignment Code
 
 var generateBtn = document.querySelector("#generate");
@@ -15,7 +31,7 @@ const key_strings = {
 };
 
 
-
+// GeneratePassword
 function generatePassword() {
   var passwordCharSet = "";
 
@@ -60,22 +76,18 @@ function generatePassword() {
     [Math.floor(Math.random() * passwordCharSet.length)]
   };
   
-  
   return password;
 }
 
-// Write password to the #password input
+// Write password to the "password Number" input
 function writePassword() {
-  var password1 = "";
-  password1 = generatePassword();
+  var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  passwordText.value = password1;
+  passwordText.value = password;
 }
 
-// function resetText(){
-//   document.getElementById("password").value = "Your Secure Password";
-// }
+// 
 
-// Add event listener to generate button
+// Add event addEventlistener to generate button a
 generateBtn.addEventListener("click", writePassword);
 });
